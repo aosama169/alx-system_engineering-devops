be7db373ef7f0b0a46f914f470fe3b9b8700ab65
Aosama <ahmed.osama@elsewedyintel.com>
2024-06-11 00:12:15 +0300

    fix lint

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,9 +4,9 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    callurl = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(callurl + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(callurl + "todos", params={"userId": sys.argv[1]}).json()
+    urlc = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(urlc + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(urlc + "todos", params={"userId": sys.argv[1]}).json()
 
     completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
